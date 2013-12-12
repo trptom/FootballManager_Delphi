5689c0c122350c267e800be6a1afb0dc79388c80
Tomas Praslicak <tpraslicak@seznam.cz>
2013-12-12 11:11:11 +0100

    Kostra pro AutoSizeTabe. Pridani Width do utils.

--- Util/Utils.pas
+++ Util/Utils.pas
@@ -5,6 +5,17 @@ interface
 uses
   SysUtils, Classes;
 
+const
+  Width_valueType_perc = 0;
+  Width_valueType_abs = 1;
+
+type
+  PWidth = ^TWidth;
+  TWidth = record
+    value: double;
+    valueType: integer;
+  end;
+
 procedure split(delimiter: Char; str: string; listOfStrings: TStrings);
 
 implementation
